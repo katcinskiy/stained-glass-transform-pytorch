ffa2ec27fd04414be44a8d794f8ed42e697ac51d
ayukhimchuk <yuximchuk99@gmail.com>
2025-08-25 08:08:48 +0300

    added batch support in precompution

--- dataset.py
+++ dataset.py
@@ -7,21 +7,29 @@ from collections import defaultdict
 from torch.utils.data import Dataset
 
 class SGTDataset(Dataset):
-    def __init__(self, texts, tokenizer, llm_for_embeds_clean_precompute=None, max_length=16):
+    def __init__(self, texts, tokenizer, llm_for_embeds_clean_precompute=None, precompution_batch_size=32, max_length=16):
         self.encodings = tokenizer(texts, truncation=True, padding='max_length', 
                                  max_length=max_length, return_tensors='pt')
         
         self.llm_for_embeds_clean_precompute = llm_for_embeds_clean_precompute
+        self.precompution_batch_size = precompution_batch_size
 
         self.cached_clean_embeds = {}
+
         if llm_for_embeds_clean_precompute is not None:
             device = next(llm_for_embeds_clean_precompute.parameters()).device
-            for idx in tqdm(range(len(self)), desc='Precomputing logits'):
-                inputs = self[idx]
+
+            for start_idx in tqdm(range(0, len(self), precompution_batch_size), desc='Precomputing logits'):
+
+                inputs = {key: val[start_idx: start_idx + precompution_batch_size] for key, val in self.encodings.items()}
                 inputs = {k: v.to(device) for k, v in inputs.items()}
                 with torch.inference_mode():
-                    self.cached_clean_embeds[idx] = llm_for_embeds_clean_precompute(input_ids=inputs['input_ids'].unsqueeze(0), attention_mask=inputs['attention_mask'].unsqueeze(0)).logits[0, :].detach().cpu()
+                    logits = llm_for_embeds_clean_precompute(input_ids=inputs['input_ids'], attention_mask=inputs['attention_mask']).logits.detach().cpu()
+
+                    for i in range(logits.shape[0]):
+                        self.cached_clean_embeds[start_idx + i] = logits[i, :]
     
+
     def __len__(self):
         return len(self.encodings.input_ids)
     
